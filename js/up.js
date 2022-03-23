ab0f4407813ebf0d8fef00eff951567065998c05
Tytenok <102251543+Tytenok@users.noreply.github.com>
2022-03-23 21:43:31 +0300

    Add files via upload
    Hey
    My first site

---
+++ js/up.js
@@ -0,0 +1,11 @@
+﻿VANTA.CELLS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+color1: 0xf0903,
+  color2: 0x5e7aa7
+})
